5a7ac338bfb623e55694b99b42349c3fe0994f6b
Ramin105 <rmnselimli@gmail.com>
2025-03-27 07:08:20 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+// let sum = 0;
+// for (i = 0; i < 100; i++) {
+//   sum += i;
+// }
+// console.log(sum);
+
+
+
+
+
+// const reqemler = [1, 2, 3, 4, 5];
+
+// for (const index in reqemler) {
+//   console.log(reqemler[index]);
+// }
+
+
+
+
+
+// const adam = { ad: "Ali", yaş: 25, şəhər: "Bakı" };
+// for (const key in adam) {
+//   console.log(key);
+// }
+
+
+
+
+// const kitab = { kitab: "1984", müəllif: "George Orwell" };
+
+// for (const key of Object.keys(kitab)) {
+//   console.log(`Açar: ${key}, Dəyər: ${kitab[key]}`);
+// }
+
+
+
+
+// const meyveler = ["alma", "armud", "banan"];
+
+// for (let i = 0; i < meyveler.length; i++) {
+//   console.log(meyveler[i]);
+// }
+
+
+
+
+// const metin = 'Salam';
+// let i = 0;
+
+// while (i < metin.length) {
+//   console.log(metin[i]);
+//   i++;
+// }
+
+
+
+
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+
+
+
+// let numbers = [5, 10, 15, 20];
+// let i = 0;
+
+// do {
+//   console.log(numbers[i]);
+//   i++;
+// } while (i < numbers.length);
+
+
+
+
+
+// let i = 1;
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 5);
